a7ba2c5303c8799addd1d2a8c973e22423cdf1b5
Tanzim <tanzim008@gmail.com>
2025-07-10 20:09:30 +0600

    added edit/update fucntion

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { useState } from "react";
 
-
 const allTasks = [];
 function App() {
   const [tasks, setTasks] = useState(allTasks);
   const [taskTitle, setTaskTitle] = useState("");
+  const [editable, setEditable] = useState(false);
+  const [editableTask, setEditableTask] = useState(null);
+
   const taskTitleHandler = (e) => {
     setTaskTitle(e.target.value);
   };
@@ -14,6 +16,9 @@ function App() {
     if (!taskTitle.trim()) {
       return alert("You have to add a task");
     }
+    editable ? updateHandler() : createHandler();
+  };
+  const createHandler = () => {
     setTasks([
       ...tasks,
       {
@@ -22,6 +27,26 @@ function App() {
       },
     ]);
     setTaskTitle("");
+  };
+  const updateHandler = () => {
+    const updatedTask = tasks.map((task)=>{
+      if(task.id === editableTask.id){
+        return{
+          ...task,
+          title: taskTitle
+        }
+      }
+      return task;
+    });
+    setTasks(updatedTask);
+    setEditable(false);
+    setTaskTitle('');
+  };
+
+  const editHandler = (task) => {
+    setEditable(true);
+    setTaskTitle(task.title);
+    setEditableTask(task);
   };
   const deleteTaskHandler = (taskID) => {
     setTasks(
@@ -33,17 +58,23 @@ function App() {
   const taskList = tasks.map((task) => {
     return (
       <li key={task.id}>
-        --
-        {task.title} 
-        <span className="icon-edit" style={{ color: "Blue" }}>Edit</span>
-        <span className="icon-delete"
+        {task.title}
+        <button
+          className="icon-edit"
+          style={{ color: "Blue" }}
+          onClick={() => editHandler(task)}
+        >
+          Edit
+        </button>
+        <button
+          className="icon-delete"
           onClick={() => {
             deleteTaskHandler(task.id);
           }}
           style={{ color: "red" }}
         >
           Delete
-        </span>
+        </button>
       </li>
     );
   });
@@ -57,7 +88,9 @@ function App() {
             value={taskTitle}
             onChange={taskTitleHandler}
           />
-          <button type="submit">Add Task</button>
+          <button type="submit">
+            {editable ? "Update Task" : "Add new Task"}
+          </button>
         </form>
         {tasks.length === 0 && "No task found"}
         <ul className="tasklists">{taskList}</ul>
